60b5f6aa1cd99613cb3d008fab1e0561a1350975
Tom Wayson <tom@tomwayson.com>
2020-03-09 17:47:16 -0700

    add styles

--- example/index.jsx
+++ example/index.jsx
@@ -2,6 +2,7 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { setDefaultOptions } from "esri-loader";
+import * as styles from "./index.css";
 import App from './components/App';
 
 // configure esri-loader
