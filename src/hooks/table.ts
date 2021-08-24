fe2d9756acacc4ac241e45f023998384e275f397
Matt Priour <mpriour@esri.com>
2021-08-24 14:23:53 -0500

    useFeatureTable - make the options object and args match the docs

--- src/hooks/table.ts
+++ src/hooks/table.ts
@@ -1,14 +1,18 @@
 import { useRef, useEffect, useState } from 'react';
 import { loadTable, destroyView } from '../utils/arcgis';
 
+interface IFeatureTableOptions {
+  portalUrl?: string;
+  [index:string]:any;
+}
+
 /**
  *
  * @param layer - Layer itemId, layer url, or layer instance
- * @param tableOptions - optional options object to configure the feature table. See https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-FeatureTable.html#properties-summary
- * @param portal - optional url to main portal entry point. Defaults to 'https://www.arcgis.com' and should only be changed if you have access to other private portals.
+ * @param options - optional options object to configure the feature table. See https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-FeatureTable.html#properties-summary
  * @returns Array[ref, table]
  */
-export function useFeatureTable(layer: any, tableOptions?: {[index:string]:any}, portal: string = 'https://www.arcgis.com') {
+export function useFeatureTable(layer: any, options?: IFeatureTableOptions) {
   // create a ref to element to be used as the table's container
   const elRef = useRef(null);
   // hold on to the table in state
@@ -16,7 +20,7 @@ export function useFeatureTable(layer: any, tableOptions?: {[index:string]:any},
   // use a ref so we can use initial values in a componentDidMount-like effect
   // otherwise we'd get a lint error, or have to make it a dependency of the effect
   // see: https://github.com/facebook/react/issues/15865#issuecomment-540715333
-  const initialArguments = useRef({ layer, tableOptions, portal });
+  const initialArguments = useRef({ layer, options });
 
   // use a side effect to create the table after react has rendered the DOM
   useEffect(() => {
@@ -24,29 +28,30 @@ export function useFeatureTable(layer: any, tableOptions?: {[index:string]:any},
     let cancelled = false;
     let _table: any;
     async function load() {
-      const { layer, tableOptions, portal } = initialArguments.current;
+      const { layer, options } = initialArguments.current;
+      const { portalUrl = 'https://www.arcgis.com', ...tableOptions} = options as IFeatureTableOptions;
       _table = await loadTable({
         layer,
-        portalUrl: portal,
+        portalUrl,
         ...tableOptions
       });
       if (cancelled) {
         return;
       }
-      // show the view at the element & add it to the state
+      // show the table at the element & add it to the state
       _table.container = elRef.current;
       setTable(_table);
     }
     load();
     return function cleanUp() {
-      // cancel any pending attempts to load the view
+      // cancel any pending attempts to load the table
       // see: https://juliangaramendy.dev/use-promise-subscription/
       cancelled = true;
-      // clean up the map view
+      // clean up the table view
       destroyView(_table);
     };
   }, []); // similar to componentDidMount(), componentWillUnmount()
 
-  // return the ref and the view
+  // return the ref and the table
   return [elRef, table];
 }
